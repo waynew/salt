4f0dd5214ccccc8cd1670c63970941ea401e2ed8
Wayne Werner <wwerner@saltstack.com>
2019-10-15 16:46:23 -0500

    Slow test report

---
+++ tools/jenkins_utils.py
@@ -0,0 +1,78 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def get_last_build(result):
+    """
+    Return the url for the most recent likely build from a Jenkins result.
+    Default to last successful build, then stable, then just last completed
+    build.
+
+    If we can't find *any* build, return None.
+    """
+    keys = ("lastSuccessfulBuild", "lastStableBuild", "lastCompletedBuild")
+    for key in keys:
+        try:
+            return result[key]["url"].rstrip("/") + "/testReport/api/json"
+        except KeyError:
+            log.error("No %r build for %r", key, result.get("name", "unknown build"))
+        except TypeError:
+            log.error("No %r build for %r", key, result.get("name", "unknown build"))
+    return None
+
+
+async def get_suite_urls(*, session, branch, jenkins_env):
+    url = f"https://{jenkins_env}.saltstack.com/job/{branch}/api/json"
+    headers = {"Accept": "application/json"}
+    log.debug("Requesting from %r", url)
+    async with session.get(url, headers=headers) as resp:
+        if resp.status != 200:
+            log.error(
+                "Failed getting results with status code %r:\n%s",
+                resp.status,
+                await resp.text(),
+            )
+        else:
+            result = await resp.json()
+            log.debug("Found %r suites", len(result["jobs"]))
+            urls = [job["url"].rstrip("/") + "/api/json" for job in result["jobs"]]
+            # Cloud build times aren't available to the public.
+            # Doc builds are irrelevant.
+            for ignored_suite in ("cloud", "docs"):
+                urls = [url for url in urls if ignored_suite not in url]
+            return urls
+
+
+async def get_most_recent_test_results(*, session, url):
+    headers = {"Accept": "application/json"}
+    async with session.get(url, headers=headers) as resp:
+        if resp.status != 200:
+            log.error(
+                "Failed getting results with status code %r:\n%s",
+                resp.status,
+                await resp.text(),
+            )
+        else:
+            result = await resp.json()
+            name = result["name"]
+            last_build = get_last_build(result)
+            if last_build is None:
+                return name, None
+            return name, await get_build_results(session=session, url=last_build)
+
+
+async def get_build_results(*, session, url):
+    headers = {"Accept": "application/json"}
+    log.error("Getting build results from %r", url)
+    async with session.get(url, headers=headers) as resp:
+        if resp.status != 200:
+            log.error("Failed to get build results from %r", url)
+        else:
+            result = await resp.json()
+            cases = []
+            for suite in result["suites"]:
+                for case in suite["cases"]:
+                    cases.append(case)
+            log.debug("Got %r cases", len(cases))
+            return cases
---
+++ tools/slow-test-report.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -
+"""
+                                 ,_-''-_,
+                 ,_-''-_,    ,_-'        '-_,
+             ,_-'        +_-'                -,
+         ,_-'            '-_,                ,+
+     ,_-'                |   '-_,        ,_-' |
+ ,_-'                ,_-'|       '-, ,_-'     |
+ '-_,                '-_,|          |         |
+ |   '-_,                '-_,       |         |
+ |       '-_,                ',     |        ,+
+ |           '-_,         ,_-'|     |    ,_-'
+ |               '-_, ,_-'    | '-_,|,_-'|
+ |                   |        |    ,_-'  |
+ |                   |        |,_-'      |
+ |                   |                   |
+ |                   |                   |
+ |                   |                   |
+ '-_,                |                ,_-'
+     '-_,            |            ,_-'
+         '-_,        |        ,_-'
+             '-_,    |    ,_-'
+                 '-_,|,_-'
+
+Slow Test Report
+================
+
+This is a somewhat simple tool that will provide the top N slowest tests in
+Salt's Jenkins environment.
+
+Run it like this:
+
+    python3 slow-test-report.py
+
+And it will spit out the 10 slowest tests. Or run it like this:
+
+    python3 slow-test-report.py 25
+
+And it will spit out the 25 slowest tests. Output is on stderr,
+so to save results just redirect to a file.
+"""
+
+import aiohttp
+import argparse
+import asyncio
+import collections
+import csv
+import io
+import logging
+import os
+import sys
+import tempfile
+
+import jenkins_utils
+
+log = logging.getLogger("slow-test-report")
+
+parser = argparse.ArgumentParser(
+    description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+)
+parser.add_argument("count", default=10, nargs="?", type=int)
+group = parser.add_mutually_exclusive_group()
+group.add_argument(
+    "--plain",
+    help="Display report in plain text, suitable for the command line.",
+    action="store_true",
+)
+group.add_argument(
+    "--tsv",
+    help="Display report as tab-separated values, suitable for piping to a file, or copy-pasting to a spreadsheet.",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "--logfile",
+    default=os.path.join(tempfile.gettempdir(), "test-report.log"),
+    type=argparse.FileType("w"),
+)
+
+
+async def collect_test_times():
+    async with aiohttp.ClientSession() as session:
+        urls = await jenkins_utils.get_suite_urls(
+            session=session, branch="master", jenkins_env="jenkinsci"
+        )
+        log.debug("Checking %r URLs", urls)
+
+        tasks = set(
+            asyncio.create_task(
+                jenkins_utils.get_most_recent_test_results(session=session, url=url)
+            )
+            for url in urls
+        )
+        test_class_results = {}
+        for comparison in asyncio.as_completed(tasks):
+            test_name, results = await comparison
+            test_class_results[test_name] = collections.Counter()
+            if results is None:
+                log.warning("No results for %r", test_name)
+            else:
+                for result in results:
+                    class_name = result["className"]
+                    test_class_results[test_name][class_name] += result["duration"]
+        return test_class_results
+
+
+def show_tsv_results(results, count):
+    headers = ["Test Run", "Test Suite", "Duration"]
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=headers, delimiter='\t')
+    writer.writeheader()
+    for test_run in results:
+        for result in results[test_run].most_common(count):
+            writer.writerow(
+                {"Test Run": test_run, "Test Suite": result[0], "Duration": f"{result[1]:.2f}"}
+            )
+    output.seek(0)
+    print(output.read())
+
+
+def show_plain_results(results, count):
+    for test_run in sorted(results):
+        print(test_run)
+        if not results[test_run]:
+            print('\tUnable to find any test runs')
+            continue
+        width = max(len(test) for test in results[test_run])
+        for test, duration in results[test_run].most_common(count):
+            print(f"\t{test:<{width}} {duration:>8.2f}")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    log = logging.getLogger()
+    log.handlers.clear()
+    sh = logging.StreamHandler(args.logfile)
+    sh.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(message)s"))
+    log.addHandler(sh)
+    log.setLevel(logging.DEBUG)
+    print("Logging to", args.logfile.name, file=sys.stderr)
+    loop = asyncio.get_event_loop()
+    test_times = loop.run_until_complete(collect_test_times())
+    if args.plain or not any((args.plain, args.tsv)):
+        show_plain_results(test_times, count=args.count)
+    elif args.tsv:
+        show_tsv_results(test_times, count=args.count)
